c869fd91367a1c0e165664796422454dcada3059
Hunter Toth <huntert98@hotmail.com>
2020-12-15 17:33:42 -0500

    Add project files.

---
+++ BlankAddin/BlankAddin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using SolidWorks.Interop.swpublished;
+using SolidWorks.Interop.sldworks;
+
+namespace BlankAddin
+{
+    [Guid("e6e9e0c7-2669-46d6-98ac-f11fdb29865b"),ComVisible(true)]
+    public class BlankAddin : SwAddin
+    {
+
+        #region Variables
+        public static BlankAddin Addin;
+        SldWorks swApp;
+        int swCookie;
+        #endregion
+
+        #region Connect/Disconnect
+        public bool ConnectToSW(object ThisSW, int Cookie)
+        {
+            swApp = (SldWorks)ThisSW;
+            Addin = this;
+            swCookie = Cookie;
+            System.Diagnostics.Debug.Print("Successfully loaded BlankAddin");
+            return true;
+        }     
+        public bool DisconnectFromSW()
+        {
+            Addin = null;
+            GC.Collect();
+            return true;
+        }
+        #endregion
+
+        #region COM_REGISTER
+        [ComRegisterFunction()]
+        public static void RegisterFunction(Type t)
+        {
+            Microsoft.Win32.RegistryKey hklm = Microsoft.Win32.Registry.LocalMachine;
+            Microsoft.Win32.RegistryKey hkcu = Microsoft.Win32.Registry.CurrentUser;
+
+            string keyname = @"SOFTWARE\SolidWorks\Addins\{" + t.GUID.ToString() + "}";
+            Microsoft.Win32.RegistryKey addinkey = hklm.CreateSubKey(keyname);
+            addinkey.SetValue(null, 0);
+            addinkey.SetValue("Description", "Description");
+            addinkey.SetValue("Title", "BlankAddin");
+
+            keyname = @"Software\SolidWorks\AddInsStartup\{" + t.GUID.ToString() + "}";
+            addinkey = hkcu.CreateSubKey(keyname);
+            addinkey.SetValue(null, 1);
+        }
+        [ComUnregisterFunction()]
+        public static void UnregisterFunction(Type t)
+        {
+            Microsoft.Win32.RegistryKey hklm = Microsoft.Win32.Registry.LocalMachine;
+            Microsoft.Win32.RegistryKey hkcu = Microsoft.Win32.Registry.CurrentUser;
+
+            string keyname = @"SOFTWARE\SolidWorks\Addins\{" + t.GUID.ToString() + "}";
+            hklm.DeleteSubKey(keyname);
+
+            keyname = @"Software\SolidWorks\AddInsStartup\{" + t.GUID.ToString() + "}";
+            hkcu.DeleteSubKey(keyname);
+        }
+        #endregion
+    }
+}
